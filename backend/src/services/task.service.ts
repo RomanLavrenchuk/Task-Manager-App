0480d4c0834eb17a9a4d54ca744800bbf446f6e4
Roman <polskaago@gmail.com>
2026-06-03 00:08:44 -0400

    add tasks service

---
+++ backend/src/services/task.service.ts
@@ -0,0 +1,43 @@
+import { Priority } from '@prisma/client';
+import {
+    createTask,
+    deleteTask,
+    getAllTasks,
+    updateTask,
+} from '../repositories/tasks.repository';
+
+export const getAllTasksService = async (userId: string) => {
+    if (!userId) {
+        throw Error('404 User not found');
+    }
+    const allTasks = await getAllTasks(userId);
+    return allTasks;
+};
+
+export const createTaskService = async (
+    name: string,
+    userId: string,
+    priority: Priority,
+    dueDate?: Date,
+) => {
+    if (!name) throw new Error('Task name is required');
+    if (!userId) throw new Error('User not found');
+
+    const newTask = await createTask(userId, name, priority, dueDate);
+    return newTask;
+};
+
+export const updateTaskService = async (
+    id: string,
+    name?: string,
+    priority?: Priority,
+    dueDate?: Date,
+) => {
+    const updatedTaskData = await updateTask(id, name, priority, dueDate);
+    return updatedTaskData;
+};
+
+export const deleteTaskService = async (id: string) => {
+    const removeTask = await deleteTask(id);
+    return removeTask;
+};
