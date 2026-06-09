acbc7a0a8db8317fc1313e84d31dcd2d47d324a4
Roman <polskaago@gmail.com>
2026-06-08 21:23:45 -0400

    add api functions for auth

---
+++ frontend/lib/api.ts
@@ -0,0 +1,29 @@
+export const API_URL =
+    process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000';
+
+export const registerUser = async (email: string, password: string) => {
+    const request = await fetch(`${API_URL}/api/auth/register`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+    });
+    if (!request.ok) {
+        throw new Error('Invalid email or password');
+    }
+    return request.json();
+};
+
+export const loginUser = async (email: string, password: string) => {
+    const request = await fetch(`${API_URL}/api/auth/login`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+    });
+    if (!request.ok) {
+        throw new Error('Bad credentials');
+    }
+    const data = await request.json();
+    return data;
+};
