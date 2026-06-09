d1df4c588460315ad9ff930418090f4512965061
Roman <polskaago@gmail.com>
2026-06-09 00:00:25 -0400

    add login form with httpOnly cookie auth

---
+++ frontend/app/(auth)/login/page.tsx
@@ -0,0 +1,5 @@
+import LoginForm from '@/components/ui/login-form';
+
+export default function page() {
+    return <LoginForm />;
+}
---
+++ frontend/components/ui/login-form.tsx
@@ -0,0 +1,46 @@
+'use client';
+import { useContextHook } from '@/context/auth-context';
+import { loginUser } from '@/lib/api';
+import { loginSchema } from '@/lib/validators';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/navigation';
+import { useForm } from 'react-hook-form';
+import z from 'zod';
+
+export default function LoginForm() {
+    const router = useRouter();
+    const { login } = useContextHook();
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm({
+        resolver: zodResolver(loginSchema),
+    });
+
+    const onSubmit = async (data: z.infer<typeof loginSchema>) => {
+        try {
+            const res = await loginUser(data.email, data.password);
+            login(res.token, res.user);
+            router.push('/dashboard');
+        } catch (e) {
+            console.log(e);
+        }
+    };
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+                <label>Email</label>
+                <input {...register('email')} />
+                {errors.email && <p>{errors.email.message}</p>}
+            </div>
+            <div>
+                <label>Password</label>
+                <input {...register('password')} />
+                {errors.password && <p>{errors.password.message}</p>}
+            </div>
+            <button>Submit</button>
+        </form>
+    );
+}
