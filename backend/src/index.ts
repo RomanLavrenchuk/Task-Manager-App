b29971888ad27486fbf4bffa4acca21a38b1b6fc
Roman <polskaago@gmail.com>
2026-07-15 22:44:43 -0400

    fix: update CORS for production

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(cookieParser()); //it allows Express read cookies
 app.use(
     cors({
-        origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+        origin: process.env.FRONTEND_URL,
         credentials: true,
     }),
 );
