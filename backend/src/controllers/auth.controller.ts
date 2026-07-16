31984f24d14957a08a536b451d6590799ce9f628
Roman <polskaago@gmail.com>
2026-07-15 23:08:29 -0400

    fix: update cookie settings for cross-domain

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -32,8 +32,8 @@ export const login = async (req: Request, res: Response) => {
         const { token, ...userWithoutToken } = user;
         res.cookie('token', token, {
             httpOnly: true,
-            secure: false,
-            sameSite: 'lax',
+            secure: true,
+            sameSite: 'none',
             maxAge: 24 * 60 * 60 * 1000,
         });
         res.status(200).json({
